41b4f3d2198c4ab9f12ab5cd13d5f66bc71e26d0
romantictrust <vladcirkunov598@gmail.com>
2019-12-10 19:58:28 +0300

    Authorization system

---
+++ CSCourseLab/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CSCourseLab.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Название категории")]
+        [MaxLength(50, ErrorMessage = "Превышена максимальная длина записи")]
+        public string Name { get; set; }
+    }
+}

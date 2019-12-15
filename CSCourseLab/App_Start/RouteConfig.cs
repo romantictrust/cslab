807be8ab082ec5ef39b5a684307ac35202bdb9d3
Vlad Tsirkunov <vladcirkunov598@gmail.com>
2019-12-15 15:29:06 +0300

    Stable version

--- CSCourseLab/App_Start/RouteConfig.cs
+++ CSCourseLab/App_Start/RouteConfig.cs
@@ -14,9 +14,9 @@ namespace CSCourseLab
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
+                name: "Request",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Request", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
